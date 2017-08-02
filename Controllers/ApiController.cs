364edbe2290a1ede141e32abc9cf54fc4bc1073e
Pieter-Paul Luijten <ppluijten@gmail.com>
2017-08-02 11:57:10 +0200

    Initial commit from ASP.NET Core initialisation from the MVC template combined with VueJs using the webpack template and making them work together using Ajax.

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VueJsAspNetCore.Controllers
+{
+    //[Produces("application/json")]
+    [Route("api")]
+    public class ApiController : Controller
+    {
+      [HttpGet]
+      [Route("hello")]
+      public IActionResult Hello()
+      {
+        var msg = new { Message = "Hello World" };
+        return this.Ok(msg);
+      }
+
+  }
+}
